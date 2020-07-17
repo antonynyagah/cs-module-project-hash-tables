581845dfec9dc664654e2a0f3a5d76b70e244600
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2020-07-16 20:29:29 -0400

    hashtable commit

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -6,22 +6,23 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
+        self.head = None
 
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
-
-
 class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity = capacity
+        self.storage = [None] * capacity  #[SLOTS]
+        self.keys = 0
 
 
     def get_num_slots(self):
@@ -29,27 +30,25 @@ class HashTable:
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
         # Your code here
+        return self.capacity
 
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
         # Your code here
+        return self.keys/self.capacity
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
@@ -59,10 +58,13 @@ class HashTable:
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
+        for byte in key:
+            hash = ((hash << 5) + hash) + ord(byte)
+        return hash
 
 
     def hash_index(self, key):
@@ -72,54 +74,97 @@ class HashTable:
         """
         #return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
-
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        hashtable = HashTableEntry(key,value)
+        node = self.storage[index]
+
+
+        if node is not None:
+            self.storage[index] = hashtable
+            self.storage[index].next = node
+        else:
+            self.storage[index] = hashtable
+            self.keys += 1
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        node = self.storage[index]
+        prev = None
+
+        if node.key == key:
+            self.storage[index] = node.next
+            return
+        while node !=None:
+            if node.key == key:
+                prev.next = node.next
+                self.storage[index].next = None
+                return
+            prev = node
+            node = node.next
+
+        return
 
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        node = self.storage[index]
+        while node is not None:
+            if node.key == key:
+                return node.value
+            else:
+                node = node.next
+
 
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
+
+        # old keys
+        old = self.storage
+        #set capacity to new capacity
+        self.capacity = new_capacity
+        #create storage with new set capacity
+        self.storage = [None] * new_capacity
+
+        #need to loop through data and add to new storage
+        for keys in old:
+            if keys:
+                current = keys
+            while current:
+                self.put(current.key, current.value)
+                current = current.next
+
+
 
 
 
 if __name__ == "__main__":
     ht = HashTable(8)
-
     ht.put("line_1", "'Twas brillig, and the slithy toves")
     ht.put("line_2", "Did gyre and gimble in the wabe:")
     ht.put("line_3", "All mimsy were the borogoves,")
@@ -132,22 +177,16 @@ if __name__ == "__main__":
     ht.put("line_10", "Long time the manxome foe he sought--")
     ht.put("line_11", "So rested he by the Tumtum tree")
     ht.put("line_12", "And stood awhile in thought.")
-
     print("")
-
     # Test storing beyond capacity
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
-
     # Test resizing
     old_capacity = ht.get_num_slots()
     ht.resize(ht.capacity * 2)
     new_capacity = ht.get_num_slots()
-
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
     # Test if data intact after resizing
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
-
     print("")
