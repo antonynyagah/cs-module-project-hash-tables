107926997a41c863277c2b2823f229459e0c5475
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2020-07-16 22:31:23 -0400

    second commit

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,20 +1,30 @@
 # Your code here
 
 
+import random
+import math
+cache ={}
+
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
     v //= (x + y)
     v %= 982451653
-
     return v
-
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    pairs = (x,y)
+    if pairs not in cache:
+        cache[pairs] = slowfun_too_slow(x,y)
+
+    return cache[pairs]
+
+
+
 
 
 
